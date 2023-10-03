b82c7396152b5e0d97a07a025b83026927a18b34
flavien <fl.lhuissier@laposte.net>
2023-10-03 09:46:00 +0200

    test Config

--- firebase/Config.js
+++ firebase/Config.js
@@ -1,18 +1,16 @@
-import * as firebase from 'firebase';
-import 'firebase/auth';
-import 'firebase/database';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAmQogCO_cBqY83d-hfc064gSZjSDZrK7k",
-    authDomain: "trelol-44eca.firebaseapp.com",
-    projectId: "trelol-44eca",
-    storageBucket: "trelol-44eca.appspot.com",
-    messagingSenderId: "12843653638",
-    appId: "1:12843653638:web:14d6f9dc02806aeb638187"
-  };
+  apiKey: "AIzaSyAmQogCO_cBqY83d-hfc064gSZjSDZrK7k",
+  authDomain: "trelol-44eca.firebaseapp.com",
+  projectId: "trelol-44eca",
+  storageBucket: "trelol-44eca.appspot.com",
+  messagingSenderId: "12843653638",
+  appId: "1:12843653638:web:14d6f9dc02806aeb638187"
+};
 
-  if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-  }
-
-  export const auth = firebase.auth();
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getDatabase(app);
